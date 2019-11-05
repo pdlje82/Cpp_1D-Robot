df6b9b5c40131fecf04104585350dd872579e788
Jan Engelke <jan.engelke@gmx.net>
2019-11-05 21:22:32 +0100

    finished main and header

--- 1D-Robot.h
+++ 1D-Robot.h
@@ -1,8 +1,14 @@
 //
 // Created by Jan Engelke on 04/11/2019.
 //
+#include<vector>
+using namespace std;
 
 #ifndef INC_01_1D_ROBOT_1D_ROBOT_H
 #define INC_01_1D_ROBOT_1D_ROBOT_H
 
 #endif //INC_01_1D_ROBOT_1D_ROBOT_H
+
+
+vector<float> sense(vector<float> p, string z );
+vector<float> move(vector<float> p, int u );
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "1D-Robot.h"
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<float> p(5, 0.2);
+    vector<string> world = {"green", "red", "red", "green", "green"};
+    vector<string> measurements = {"red", "green"};
+    vector<int> motions = {1, 1};
+    float pHit = 0.6;
+    float pMiss = 0.2;
+    float pExact = 0.8;
+    float pOvershoot = 0.1;
+    float pUndershoot = 0.1;
+
+    for (int k = 0; k < measurements.size(); k++ ){
+        p = sense(p, measurements[k]);
+        p = move(p, motions[k]);
+    }
+    for (int i = 0; i < p.size(); i++ ){
+        cout << p[i] << endl;
+    }
     return 0;
 }
