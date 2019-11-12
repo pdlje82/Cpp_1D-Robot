feb8f48872b503f51fa77741af410c36c6240006
Jan Engelke <jan.engelke@gmx.net>
2019-11-12 23:54:19 +0100

    corrected stuff

--- 1D-Robot.h
+++ 1D-Robot.h
@@ -1,20 +1,21 @@
 //
 // Created by Jan Engelke on 04/11/2019.
 //
-using namespace std;
 
 #ifndef INC_01_1D_ROBOT_1D_ROBOT_H
 #define INC_01_1D_ROBOT_1D_ROBOT_H
-#endif //INC_01_1D_ROBOT_1D_ROBOT_H
 
-vector<float> sense(vector<float> p,
-                    string z,
-                    vector<string> world,
+std::vector<float> sense(std::vector<float> p,
+                         std::string z,
+                         std::vector<std::string> world,
                     float pHit,
                     float pMiss );
-vector<float> move(vector<float> p,
-                    int u,
-                    float pExact,
-                    float pOvershoot,
-                    float pUndershoot );
-void print_p(vector<float> p);
+std::vector<float> move(std::vector<float> p,
+                   int u,
+                   float pExact,
+                   float pOvershoot,
+                   float pUndershoot );
+void print_p(std::vector<float> p);
+
+#endif //INC_01_1D_ROBOT_1D_ROBOT_H
+
